461f40f1d24a8215d8623de6cc1a1d81fc91bb8e
Zach Norgaard <znorgaard@bionano.com>
2023-12-01 12:53:03 -0800

    feat: day one solution

--- src/main/scala/advent/days/DayOne.scala
+++ src/main/scala/advent/days/DayOne.scala
@@ -1,7 +1,68 @@
 package advent
 package days
 
+import scala.io.{BufferedSource, Source}
+import scala.util.matching.Regex
+
+
 object DayOne {
     def main(): Unit =
-        println("Hello day one.")
+        // Part Two Solution
+        val puzzleInput: BufferedSource = Source.fromFile("src/main/resources/DayOneInput.txt")
+        val total: Int = puzzleInput
+          .getLines()
+          .map(doubleDigitIntFromLine)
+          .sum
+        println(total)
+        puzzleInput.close()
+
+    def partOne(): Unit =
+        val puzzleInput: BufferedSource = Source.fromFile("src/main/resources/DayOneInput.txt")
+        val total: Int = puzzleInput
+          .getLines()
+          .map(getDigits)
+          .map(doubleDigitInt)
+          .sum
+        println(total)
+        puzzleInput.close()
+
+    def getDigits(line: String): Seq[String] =
+        line.filter(_.isDigit).split("").toSeq
+
+    def doubleDigitInt(digits: Seq[String]): Int =
+        (digits.head + digits.last).toInt
+
+    def getDigitIndices(line: String): Map[String, Seq[Int]] =
+        digitsMap.map(
+            digitPair => {
+                val word: String = digitPair._1
+                val digit: String = digitPair._2
+                digit -> Regex(word + "|" + digit).findAllMatchIn(line).map(_.start).toSeq
+            }
+        )
+
+    def firstDigit(digitIndices: Map[String, Seq[Int]]): String =
+        val minIndex = digitIndices.values.flatten.min
+        digitIndices.view.filterKeys(digitIndices(_) contains minIndex).keys.head
+
+    def lastDigit(digitIndices: Map[String, Seq[Int]]): String =
+        val maxIndex = digitIndices.values.flatten.max
+        digitIndices.view.filterKeys(digitIndices(_) contains maxIndex).keys.head
+
+    def doubleDigitIntFromLine(line: String): Int =
+        val digitIndices = getDigitIndices(line)
+        (firstDigit(digitIndices) + lastDigit(digitIndices)).toInt
+
+    private val digitsMap: Map[String, String] = Map(
+        "one" -> "1",
+        "two" -> "2",
+        "three" -> "3",
+        "four" -> "4",
+        "five" -> "5",
+        "six" -> "6",
+        "seven" -> "7",
+        "eight" -> "8",
+        "nine" -> "9"
+    )
+
 }
--- src/test/scala/days/DayOneTests.scala
+++ src/test/scala/days/DayOneTests.scala
@@ -1,10 +1,138 @@
+package advent
 package days
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
 
 class DayOneTests extends AnyFunSuite {
+  val testInput: Seq[String] = Seq("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet")
 
-  // First test
-  test("first test")(pending)
+  test("getDigits") {
+    val result: Seq[Seq[String]] = testInput.map(line => DayOne.getDigits(line))
+    result should equal(Seq(Seq("1", "2"), Seq("3", "8"), Seq("1", "2", "3", "4", "5"), Seq("7")))
+  }
+
+  test("doubleDigitInt") {
+    val result: Seq[Int] = testInput.map(line => DayOne.getDigits(line))
+      .map(digits => DayOne.doubleDigitInt(digits))
+    result should equal(Seq(12, 38, 15, 77))
+  }
+
+  val test2Input: Seq[String] = Seq(
+    "two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"
+  )
+
+  test("getDigitIndices") {
+    val result: Seq[Map[String, Seq[Int]]] = test2Input.map(line => DayOne.getDigitIndices(line))
+    val expectedResults: Seq[Map[String, Seq[Int]]] = Seq(
+      Map( // two1nine
+        "1" -> Seq(3),
+        "2" -> Seq(0),
+        "3" -> Seq(),
+        "4" -> Seq(),
+        "5" -> Seq(),
+        "6" -> Seq(),
+        "7" -> Seq(),
+        "8" -> Seq(),
+        "9" -> Seq(4)
+      ),
+      Map(
+        // eightwothree
+        "1" -> Seq(),
+        "2" -> Seq(4),
+        "3" -> Seq(7),
+        "4" -> Seq(),
+        "5" -> Seq(),
+        "6" -> Seq(),
+        "7" -> Seq(),
+        "8" -> Seq(0),
+        "9" -> Seq()
+      ),
+      Map(
+        // abcone2threexyz
+        "1" -> Seq(3),
+        "2" -> Seq(6),
+        "3" -> Seq(7),
+        "4" -> Seq(),
+        "5" -> Seq(),
+        "6" -> Seq(),
+        "7" -> Seq(),
+        "8" -> Seq(),
+        "9" -> Seq()
+      ),
+      Map(
+        // xtwone3four
+        "1" -> Seq(3),
+        "2" -> Seq(1),
+        "3" -> Seq(6),
+        "4" -> Seq(7),
+        "5" -> Seq(),
+        "6" -> Seq(),
+        "7" -> Seq(),
+        "8" -> Seq(),
+        "9" -> Seq()
+      ),
+      Map(
+        // 4nineeightseven2
+        "1" -> Seq(),
+        "2" -> Seq(15),
+        "3" -> Seq(),
+        "4" -> Seq(0),
+        "5" -> Seq(),
+        "6" -> Seq(),
+        "7" -> Seq(10),
+        "8" -> Seq(5),
+        "9" -> Seq(1)
+      ),
+      Map(
+        // zoneight234
+        "1" -> Seq(1),
+        "2" -> Seq(8),
+        "3" -> Seq(9),
+        "4" -> Seq(10),
+        "5" -> Seq(),
+        "6" -> Seq(),
+        "7" -> Seq(),
+        "8" -> Seq(3),
+        "9" -> Seq()
+      ),
+      Map(
+        // 7pqrstsixteen
+        "1" -> Seq(),
+        "2" -> Seq(),
+        "3" -> Seq(),
+        "4" -> Seq(),
+        "5" -> Seq(),
+        "6" -> Seq(6),
+        "7" -> Seq(0),
+        "8" -> Seq(),
+        "9" -> Seq()
+      )
+    )
+    result.indices.foreach(
+      n => {
+        result(n) should contain theSameElementsAs expectedResults(n)
+      }
+    )
+  }
+
+  test("firstDigit") {
+    DayOne.firstDigit(Map("1" -> Seq(), "2" -> Seq(0))) shouldBe "2"
+    DayOne.firstDigit(Map("1" -> Seq(0), "2" -> Seq())) shouldBe "1"
+  }
+
+  test("lastDigit") {
+    DayOne.lastDigit(Map("1" -> Seq(1), "2" -> Seq(0))) shouldBe "1"
+    DayOne.lastDigit(Map("1" -> Seq(0), "2" -> Seq(1))) shouldBe "2"
+  }
+
+  test("doubleDigitIntFromLine") {
+    //val test2Input: Seq[String] = Seq(
+    //    "two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"
+    //  )
+    val result: Seq[Int] = test2Input.map(line => DayOne.doubleDigitIntFromLine(line))
+    result should equal(Seq(29, 83, 13, 24, 42, 14, 76))
+
+  }
 
 }

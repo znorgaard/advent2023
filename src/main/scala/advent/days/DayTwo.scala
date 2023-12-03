c108a1cbeb331f1078e97002ff34d0c1ddb97fbf
Zach Norgaard <znorgaard@bionano.com>
2023-12-03 10:11:19 -0800

    feat: add day two

--- src/main/scala/advent/days/DayTwo.scala
+++ src/main/scala/advent/days/DayTwo.scala
@@ -1,6 +1,58 @@
 package advent.days
 
+import java.awt.GridBagConstraints
+import scala.io.{BufferedSource, Source}
+
 object DayTwo {
     def main(): Unit =
-        println("Hello day two.")
+        val puzzleInput: BufferedSource = Source.fromFile("src/main/resources/DayTwoInput.txt")
+        val total: Int = sumPower(puzzleInput.getLines())
+        println(total)
+
+    def partOne(): Unit =
+        val puzzleInput: BufferedSource = Source.fromFile("src/main/resources/DayTwoInput.txt")
+        val bagContents: Map[String, Int] = Map("red" -> 12, "green" -> 13, "blue" -> 14)
+        val total: Int = sumGames(puzzleInput.getLines(), bagContents)
+        println(total)
+
+
+    def maxDraws(line: String): (Int, Map[String, Int]) =
+        val game: Seq[String] = line.split(": ")
+        val gameNumber: Int = game.head.split(" ").last.toInt
+        val draws: Seq[Map[String, Int]] = game.last.split("; ")
+          .map(draw => draw.split(", ").map(
+            color => {
+                val countColorSeq: Seq[String] = color.split(" ")
+                countColorSeq.last -> countColorSeq.head.toInt
+            }).toMap
+        )
+        val maxCounts = draws.flatMap(_.keys).distinct.map(
+            color => {
+                color -> draws.map(_.getOrElse(color, 0)).max
+            }
+        ).toMap
+        gameNumber -> maxCounts
+
+    def possible(bagContents: Map[String, Int], maxCounts: Map[String, Int]): Boolean =
+        bagContents.keys.map(
+            color => {
+                bagContents(color) >= maxCounts.getOrElse(color, 0)
+            }
+        ).forall(identity)
+
+    def sumGames(lines: Iterator[String], bagContents: Map[String, Int]): Int =
+        lines
+          .map(maxDraws)
+          .filter(gameTuple => possible(bagContents, gameTuple._2))
+          .map(_._1)
+          .sum
+
+    def gamePower(maxCounts: Map[String, Int]): Int =
+        maxCounts.values.product
+
+    def sumPower(lines: Iterator[String]): Int =
+        lines.map(maxDraws)
+          .map(gameTuple => gamePower(gameTuple._2))
+          .sum
+
 }
--- src/test/scala/days/DayTwoTests.scala
+++ src/test/scala/days/DayTwoTests.scala
@@ -1,10 +1,52 @@
+package advent
 package days
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
 
 class DayTwoTests extends AnyFunSuite {
+  val testInput: Seq[String] = Seq(
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+  )
 
   // First test
-  test("first test")(pending)
+  test("maxDraws") {
+    val expectedMaxes: Seq[(Int, Map[String, Int])] = Seq(
+      (1 -> Map("blue" ->  6, "red" ->  4, "green" ->  2)),
+      (2 -> Map("blue" ->  4, "red" ->  1, "green" ->  3)),
+      (3 -> Map("blue" ->  6, "red" -> 20, "green" -> 13)),
+      (4 -> Map("blue" -> 15, "red" -> 14, "green" ->  3)),
+      (5 -> Map("blue" ->  2, "red" ->  6, "green" ->  3))
+    )
+    testInput.map(line => DayTwo.maxDraws(line)) should equal(expectedMaxes)
+  }
+
+  val bagContents: Map[String, Int] = Map("red" -> 12, "green" -> 13, "blue" -> 14)
+  test("possible") {
+    DayTwo.possible(bagContents, Map("blue" ->  6, "red" ->  4, "green" ->  2)) shouldBe true
+    DayTwo.possible(bagContents, Map("blue" ->  4, "red" ->  1, "green" ->  3)) shouldBe true
+    DayTwo.possible(bagContents, Map("blue" ->  6, "red" -> 20, "green" -> 13)) shouldBe false
+    DayTwo.possible(bagContents, Map("blue" -> 15, "red" -> 14, "green" ->  3)) shouldBe false
+    DayTwo.possible(bagContents, Map("blue" ->  2, "red" ->  6, "green" ->  3)) shouldBe true
+  }
+
+  test("sumGames") {
+    DayTwo.sumGames(testInput.iterator, bagContents) shouldBe 8
+  }
+  test("gamePower") {
+    DayTwo.gamePower(Map("blue" -> 6, "red" -> 4, "green" -> 2)) shouldBe 48
+    DayTwo.gamePower(Map("blue" -> 4, "red" -> 1, "green" -> 3)) shouldBe 12
+    DayTwo.gamePower(Map("blue" -> 6, "red" -> 20, "green" -> 13)) shouldBe 1560
+    DayTwo.gamePower(Map("blue" -> 15, "red" -> 14, "green" -> 3)) shouldBe 630
+    DayTwo.gamePower(Map("blue" -> 2, "red" -> 6, "green" -> 3)) shouldBe 36
+  }
+
+  test("sumPower") {
+    DayTwo.sumPower(testInput.iterator) shouldBe 2286
+  }
 
 }

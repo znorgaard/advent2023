debbcff9d4af6e60e9ff947bbbbc4dfe2abc9fea
Zach Norgaard <znorgaard@bionano.com>
2023-12-06 13:43:45 -0800

    fead: solve day 6

--- src/main/scala/advent/days/DaySix.scala
+++ src/main/scala/advent/days/DaySix.scala
@@ -1,6 +1,45 @@
 package advent.days
 
+import scala.io.{BufferedSource, Source}
+
 object DaySix {
+    // Could be improved by:
+    //   find the first charge time that beats the required distance then
+    //      winOptions = time - (firstWinningTime * 2) + 1
+    val puzzleInput: BufferedSource = Source.fromFile("src/main/resources/DaySixInput.txt")
+
     def main(): Unit =
-        println("Hello day six.")
+        val lines: Iterator[String] = puzzleInput
+          .getLines()
+        val raceConditions: Seq[(Int, Int)] = readLines(lines)
+        val wins: Seq[Int] = raceConditions.map(race => winOptions(race.head, race.last))
+        val total: Int = wins.product
+        println(total)
+        println(longOptions(46857582, "208141212571410".toLong))
+
+    def readLines(lines: Iterator[String]): Seq[(Int, Int)] =
+        val times = "Time:\\s+".r.replaceAllIn(lines.next, "").split("\\s+").map(_.toInt)
+        val distances = "Distance:\\s+".r.replaceAllIn(lines.next, "").split("\\s+").map(_.toInt)
+        times.zip(distances)
+
+    def distances(time: Int): Seq[Int] =
+        (0 to time).map(
+            chargeTime => {
+                chargeTime * (time - chargeTime)
+            }
+        )
+
+    def winOptions(time: Int, distance: Int): Int =
+        distances(time).count(_ > distance)
+
+    def longDistances(time: Long): Seq[Long] =
+        (0L to time).map(
+            chargeTime => {
+                chargeTime * (time - chargeTime)
+            }
+        )
+    def longOptions(time: Long, distance: Long): Int =
+        longDistances(time).count(_ > distance)
+
+
 }
--- src/test/scala/days/DaySixTests.scala
+++ src/test/scala/days/DaySixTests.scala
@@ -1,10 +1,31 @@
+package advent
 package days
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
 
 class DaySixTests extends AnyFunSuite {
 
+  val testInput: Seq[String] = Seq(
+    "Time:      7  15   30",
+    "Distance:  9  40  200"
+  )
+
   // First test
-  test("first test")(pending)
+  test("readLines") {
+    DaySix.readLines(testInput.iterator) should equal(Seq((7, 9), (15, 40), (30, 200)))
+  }
+
+  test("distances") {
+    DaySix.distances(7) should equal(Seq(0, 6, 10, 12, 12, 10, 6, 0))
+  }
+
+  test("winOptions") {
+    DaySix.winOptions(7, 9) shouldBe 4
+    DaySix.winOptions(15, 40) shouldBe 8
+    DaySix.winOptions(30, 200) shouldBe 9
+    DaySix.winOptions(71530, 940200) shouldBe 71503
+  }
+
 
 }

b4d0b47d43f141c5586bc9ff66e52036e285b516
Zach Norgaard <znorgaard@bionano.com>
2023-12-08 08:20:48 -0800

    feat: solve day 7

--- src/main/scala/advent/days/DaySeven.scala
+++ src/main/scala/advent/days/DaySeven.scala
@@ -1,6 +1,105 @@
 package advent.days
 
+import scala.io.{BufferedSource, Source}
 object DaySeven {
+    val puzzleInput: BufferedSource = Source.fromFile("src/main/resources/DaySevenInput.txt")
+
     def main(): Unit =
-        println("Hello day seven.")
+        val lines: Iterator[String] = puzzleInput
+          .getLines()
+        //val total: Int = partOne(lines)
+        val total: Int = partTwo(lines)
+        println(total)
+
+    def partOne(lines: Iterator[String]): Int =
+        val handBidPairs: Map[Hand, Int] = lines.map(readLine).toMap // Confirmed no duplicate hands in input
+        val sortedHands: Seq[Hand] = handBidPairs.keys.toSeq.sorted(HandOrdering)
+        sortedHands.zipWithIndex.map(
+            handAndIndex => {
+                handBidPairs(handAndIndex._1) * (handAndIndex._2 + 1)
+            }
+        ).sum
+
+    def partTwo(lines: Iterator[String]): Int =
+        val handBidPairs: Map[Hand, Int] = lines.map(readJokerLine).toMap // Confirmed no duplicate hands in input
+        val sortedHands: Seq[Hand] = handBidPairs.keys.toSeq.sorted(JokerHandOrdering)
+        sortedHands.zipWithIndex.map(
+            handAndIndex => {
+                handBidPairs(handAndIndex._1) * (handAndIndex._2 + 1)
+            }
+        ).sum
+
+    private val cardMap: Map[String, Int] =
+        Map("A" -> 14, "K" -> 13, "Q" -> 12, "J" -> 11, "T" -> 10) ++ (2 to 9).map(x => x.toString -> x)
+
+    val jokerCardMap: Map[String, Int] =
+        Map("A" -> 14, "K" -> 13, "Q" -> 12, "J" -> 0, "T" -> 10) ++ (2 to 9).map(x => x.toString -> x)
+    def buildHand(handString: String, cMap: Map[String, Int] = cardMap): Seq[Int] =
+        handString.split("").map(card => cMap(card))
+
+    def rankHand(hand: Seq[Int]): Int =
+        val cardCounts: Seq[Int] = hand.groupBy(identity).view.mapValues(_.size).values.toSeq
+        cardCounts match {
+            case fiveOfAKind if fiveOfAKind contains 5 => 7
+            case fourOfAKind if fourOfAKind contains 4 => 6
+            case fullHouse if Seq(2, 3).forall(fullHouse.contains) => 5
+            case threeOfAKind if threeOfAKind contains 3 => 4
+            case twoPair if twoPair.count(_ == 2) == 2 => 3
+            case onePair if onePair contains 2 => 2
+            case _ => 1
+        }
+
+    def rankJokerHand(hand: Seq[Int]): Int =
+        val cardCountMap: Map[Int, Int] = hand.groupBy(identity).view.mapValues(_.size).toMap
+        val nonZeroMap: Map[Int, Int] = cardCountMap.view.filterKeys(_ != 0).toMap
+        val cardCounts = if (nonZeroMap.isEmpty) {
+            Seq(cardCountMap(0))
+        } else {
+            val maxNonZeroCard: (Int, Int) = nonZeroMap.find(_._2 == nonZeroMap.values.max).get
+            cardCountMap.view.filterKeys(key => !Seq(0, maxNonZeroCard._1).contains(key)).values.toSeq :+
+              (maxNonZeroCard._2 + cardCountMap.getOrElse(0, 0))
+        }
+        cardCounts match {
+            case fiveOfAKind if fiveOfAKind contains 5 => 7
+            case fourOfAKind if fourOfAKind contains 4 => 6
+            case fullHouse if Seq(2, 3).forall(fullHouse.contains) => 5
+            case threeOfAKind if threeOfAKind contains 3 => 4
+            case twoPair if twoPair.count(_ == 2) == 2 => 3
+            case onePair if onePair contains 2 => 2
+            case _ => 1
+        }
+
+    def readLine(line: String): (Seq[Int], Int) =
+        val splitLine = line.split(" ")
+        (buildHand(splitLine.head), splitLine.last.toInt)
+
+    def readJokerLine(line: String): (Seq[Int], Int) =
+        val splitLine = line.split(" ")
+        (buildHand(splitLine.head, jokerCardMap), splitLine.last.toInt)
+
+}
+
+type Hand = Seq[Int]
+object HandOrdering extends Ordering[Hand] {
+    def compare(a: Hand, b: Hand): Int = {
+        val aRank: Int = DaySeven.rankHand(a)
+        val bRank: Int = DaySeven.rankHand(b)
+        if (aRank != bRank) {
+            aRank compare bRank
+        } else {
+            a.zipWithIndex.map(pair => pair._1 compare b(pair._2)).find(_ != 0).getOrElse(0)
+        }
+    }
+}
+
+object JokerHandOrdering extends Ordering[Hand] {
+    def compare(a: Hand, b: Hand): Int = {
+        val aRank: Int = DaySeven.rankJokerHand(a)
+        val bRank: Int = DaySeven.rankJokerHand(b)
+        if (aRank != bRank) {
+            aRank compare bRank
+        } else {
+            a.zipWithIndex.map(pair => pair._1 compare b(pair._2)).find(_ != 0).getOrElse(0)
+        }
+    }
 }
--- src/test/scala/days/DaySevenTests.scala
+++ src/test/scala/days/DaySevenTests.scala
@@ -1,10 +1,102 @@
+package advent
 package days
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
 
 class DaySevenTests extends AnyFunSuite {
+  val testInput: Seq[String] = Seq(
+    "32T3K 765",
+    "T55J5 684",
+    "KK677 28",
+    "KTJJT 220",
+    "QQQJA 483"
+  )
 
   // First test
-  test("first test")(pending)
+  test("buildHand") {
+    DaySeven.buildHand("AAAAA") shouldBe Seq(14, 14, 14, 14, 14)
+    DaySeven.buildHand("AA8AA") shouldBe Seq(14, 14, 8, 14, 14)
+    DaySeven.buildHand("23456") shouldBe Seq(2, 3, 4, 5 ,6)
+    DaySeven.buildHand("AAAAA", DaySeven.jokerCardMap) shouldBe Seq(14, 14, 14, 14, 14)
+    DaySeven.buildHand("AA8AJ") shouldBe Seq(14, 14, 8, 14, 11)
+    DaySeven.buildHand("AA8AJ", DaySeven.jokerCardMap) shouldBe Seq(14, 14, 8, 14, 0)
+  }
 
+  test("rankHand") {
+    DaySeven.rankHand(Seq(14, 14, 14, 14, 14)) shouldBe 7
+    DaySeven.rankHand(Seq(14, 13, 14, 14, 14)) shouldBe 6
+    DaySeven.rankHand(Seq(14, 14, 12, 12, 12)) shouldBe 5
+    DaySeven.rankHand(Seq(14, 13, 12, 12, 12)) shouldBe 4
+    DaySeven.rankHand(Seq(14, 13, 13, 11, 11)) shouldBe 3
+    DaySeven.rankHand(Seq(14, 13, 12, 11, 11)) shouldBe 2
+    DaySeven.rankHand(Seq(14, 13, 12, 11, 10)) shouldBe 1
+  }
+
+  test("rankJokerHand") {
+    DaySeven.rankJokerHand(Seq(14, 14, 14, 0, 14)) shouldBe 7
+    DaySeven.rankJokerHand(Seq(14, 13, 14, 0, 14)) shouldBe 6
+    DaySeven.rankJokerHand(Seq(14, 14, 12, 0, 12)) shouldBe 5
+    DaySeven.rankJokerHand(Seq(14, 13, 12, 0, 12)) shouldBe 4
+    DaySeven.rankJokerHand(Seq(14, 13, 12, 0, 11)) shouldBe 2
+    DaySeven.rankJokerHand(Seq(0, 0, 0, 0, 0)) shouldBe 7
+    DaySeven.rankJokerHand(Seq(14, 0, 0, 0, 0)) shouldBe 7
+  }
+
+  test("readLine") {
+    val parsedLines = Seq(
+      (Seq(3, 2, 10, 3, 13), 765),
+      (Seq(10, 5, 5, 11, 5), 684),
+      (Seq(13, 13, 6, 7, 7), 28),
+      (Seq(13, 10, 11, 11, 10), 220),
+      (Seq(12, 12, 12, 11, 14), 483)
+    )
+    testInput.map(DaySeven.readLine) should equal(parsedLines)
+  }
+
+  test("HandOrdering") {
+    val hands = Seq(
+      Seq(3, 2, 10, 3, 13),
+      Seq(10, 5, 5, 11, 5),
+      Seq(13, 13, 6, 7, 7),
+      Seq(13, 10, 11, 11, 10),
+      Seq(12, 12, 12, 11, 14)
+    )
+    val sortedHands = Seq(
+      Seq(3, 2, 10, 3, 13),
+      Seq(13, 10, 11, 11, 10),
+      Seq(13, 13, 6, 7, 7),
+      Seq(10, 5, 5, 11, 5),
+      Seq(12, 12, 12, 11, 14)
+    )
+    hands.sorted(HandOrdering) should equal(sortedHands)
+  }
+
+  test("JokerHandOrdering") {
+    val hands = Seq(
+      Seq(3, 2, 10, 3, 13),
+      Seq(10, 5, 5, 0, 5),
+      Seq(13, 13, 6, 7, 7),
+      Seq(13, 10, 0, 0, 10),
+      Seq(12, 12, 12, 0, 14)
+    )
+    val sortedHands = Seq(
+      Seq(3, 2, 10, 3, 13),
+      Seq(13, 13, 6, 7, 7),
+      Seq(10, 5, 5, 0, 5),
+      Seq(12, 12, 12, 0, 14),
+      Seq(13, 10, 0, 0, 10)
+    )
+    hands.sorted(JokerHandOrdering) should equal(sortedHands)
+  }
+
+  test("partOne") {
+    DaySeven.partOne(Iterator("32T3K 765")) shouldBe 765
+    DaySeven.partOne(testInput.iterator) shouldBe 6440
+  }
+
+  test("partTwo") {
+    DaySeven.partTwo(Iterator("32T3K 765")) shouldBe 765
+    DaySeven.partTwo(testInput.iterator) shouldBe 5905
+  }
 }

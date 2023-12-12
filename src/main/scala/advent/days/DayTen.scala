b89a0648b75e6f3ce351df3d2ff1e7d72bb62c00
Zach Norgaard <znorgaard@bionano.com>
2023-12-11 16:54:00 -0800

    feat: solve day 10 part 1

--- src/main/scala/advent/days/DayTen.scala
+++ src/main/scala/advent/days/DayTen.scala
@@ -1,6 +1,107 @@
 package advent.days
 
+import scala.io.{BufferedSource, Source}
+
 object DayTen {
+    val puzzleInput: BufferedSource = Source.fromFile("src/main/resources/DayTenInput.txt")
     def main(): Unit =
-        println("Hello day ten.")
+        val lines: Iterator[String] = puzzleInput
+          .getLines()
+        buildLoops(getMatrix(lines.toSeq))
+
+
+    val symbolCodes = Map(
+        // Direction codes
+        //   1
+        // 0 + 2
+        //   3
+        "|" -> Seq(1, 3),
+        "-" -> Seq(0, 2),
+        "L" -> Seq(1, 2),
+        "J" -> Seq(0, 1),
+        "7" -> Seq(0, 3),
+        "F" -> Seq(2, 3),
+        "." -> Seq.empty,
+        "S" -> Seq.empty
+    )
+
+    val movementCodes = Map(
+        0 -> ( 0, -1), // Same row, previous column
+        1 -> (-1,  0), // Previous row, same column
+        2 -> ( 0,  1), // Same row, next column
+        3 -> ( 1,  0)  // Next row, same column
+    )
+
+    def getMatrix(lines: Seq[String]): Seq[Seq[String]] =
+        lines.map(_.split(""))
+
+    def findAnimal(matrix: Seq[Seq[String]]): (Int, Int) =
+        matrix.zipWithIndex.flatMap((row, rowIndex) => {
+            if (row.contains("S")) {
+                Some((rowIndex, row.indexOf("S")))
+            } else
+                None
+        }).head
+
+
+    def checkConnection(symbol: String, from: Int): Boolean =
+        symbol == "S" || symbolCodes(symbol).contains(from)
+
+    def nextPosition(currentPostion: (Int, Int), symbol: String, from: Int): (Int, Int) =
+        val movement = movementCodes(whereTo(symbol, from))
+        (currentPostion._1 + movement._1, currentPostion._2 + movement._2)
+
+    def whereTo(symbol: String, from: Int): Int =
+        symbolCodes(symbol).filter(_ != from).head
+
+    def whereFrom(toDirection: Int): Int =
+        Map(0 -> 2, 1 -> 3, 2 -> 0, 3 -> 1)(toDirection)
+
+    def getSymbol(matrix: Seq[Seq[String]], position: (Int, Int)): String =
+        matrix(position._1)(position._2)
+
+    def buildLoop(matrix: Seq[Seq[String]], currentPostion: (Int, Int), from: Int): Seq[(Int, Int)] =
+        val currentSymbol: String = getSymbol(matrix, currentPostion)
+        val connectionCheck: Boolean = checkConnection(currentSymbol, from)
+        if (currentSymbol == "." || !connectionCheck) {
+            Seq.empty // Not part of a loop, we're done!
+        } else if (currentSymbol == "S") {
+            Seq(currentPostion) // We looped, we're done!
+        } else {
+            val nextPostion = nextPosition(currentPostion, currentSymbol, from)
+            val nextFrom = whereFrom(whereTo(currentSymbol, from))
+            Seq(currentPostion) ++ buildLoop(matrix, nextPostion, nextFrom)
+        }
+
+    def buildLoops(matrix: Seq[Seq[String]]): Unit =
+        val startLocation = findAnimal(matrix)
+        // Technically only need to check until you find the full loop, but oh well
+        // Go North
+        val northLoop = buildLoop(
+            matrix,
+            (startLocation._1 - 1, startLocation._2),
+            3
+        )
+        // Go South
+        val southLoop = buildLoop(
+            matrix,
+            (startLocation._1 + 1, startLocation._2),
+            1
+        )
+        // Go West
+        val westLoop = buildLoop(
+            matrix,
+            (startLocation._1, startLocation._2 + 1),
+            0
+        )
+        // Go East
+        val eastLoop = buildLoop(
+            matrix,
+            (startLocation._1, startLocation._2 - 1),
+            2
+        )
+        val loops: Seq[Seq[(Int, Int)]] = Seq(northLoop, eastLoop, southLoop, westLoop)
+        println(loops.map(_.length).max / 2)
+
+
 }
--- src/test/scala/days/DayTenTests.scala
+++ src/test/scala/days/DayTenTests.scala
@@ -1,10 +1,127 @@
+package advent
 package days
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
 
 class DayTenTests extends AnyFunSuite {
+  val simpleTextInput: Seq[String] = Seq(
+    ".....",
+    ".S-7.",
+    ".|.|.",
+    ".L-J.",
+    "....."
+  )
+  val simplestInput: Seq[Seq[String]] = Seq(
+    ".....".split("").toSeq,
+    ".S-7.".split("").toSeq,
+    ".|.|.".split("").toSeq,
+    ".L-J.".split("").toSeq,
+    ".....".split("").toSeq
+  )
+
+  val simpleInput: Seq[Seq[String]] = Seq(
+    "-L|F7".split("").toSeq,
+    "7S-7|".split("").toSeq,
+    "L|7||".split("").toSeq,
+    "-L-J|".split("").toSeq,
+    "L|-JF".split("").toSeq
+  )
+
+  val mediumInput: Seq[Seq[String]] = Seq(
+    "..F7.".split("").toSeq,
+    ".FJ|.".split("").toSeq,
+    "SJ.L7".split("").toSeq,
+    "|F--J".split("").toSeq,
+    "LJ...".split("").toSeq
+  )
+
+  val hardInput: Seq[Seq[String]] = Seq(
+    "7-F7-".split("").toSeq,
+    ".FJ|7".split("").toSeq,
+    "SJLL7".split("").toSeq,
+    "|F--J".split("").toSeq,
+    "LJ.LJ".split("").toSeq
+  )
 
   // First test
-  test("first test")(pending)
+  test("getMatrix") {
+    DayTen.getMatrix(simpleTextInput) shouldBe simplestInput
+  }
+
+  test("findAnimal") {
+    DayTen.findAnimal(simplestInput) shouldBe (1,1)
+    DayTen.findAnimal(simpleInput) shouldBe (1,1)
+    DayTen.findAnimal(mediumInput) shouldBe (2,0)
+    DayTen.findAnimal(hardInput) shouldBe (2,0)
+  }
+
+  test("checkConnection") {
+    DayTen.checkConnection("S", 1) shouldBe true
+    DayTen.checkConnection("|", 1) shouldBe true
+    DayTen.checkConnection("|", 3) shouldBe true
+    DayTen.checkConnection("|", 2) shouldBe false
+    DayTen.checkConnection("L", 1) shouldBe true
+    DayTen.checkConnection("L", 2) shouldBe true
+  }
+
+  test("nextPosition") {
+    DayTen.nextPosition((0,0), "F", 3) shouldBe (0,1)
+    DayTen.nextPosition((1,1), "|", 1) shouldBe (2,1)
+    DayTen.nextPosition((1,1), "-", 0) shouldBe (1,2)
+    DayTen.nextPosition((1,1), "-", 2) shouldBe (1,0)
+    DayTen.nextPosition((1,1), "L", 1) shouldBe (1,2)
+    DayTen.nextPosition((1,1), "J", 1) shouldBe (1,0)
+    DayTen.nextPosition((1,1), "7", 0) shouldBe (2,1)
+  }
+
+  test("whereTo") {
+    DayTen.whereTo("-", 0) shouldBe 2
+  }
+
+  test("getSymbol") {
+    DayTen.getSymbol(Seq(Seq("S", "J"), Seq("7", "L")), (0,1)) shouldBe "J"
+  }
+
+  test("buildLoop") {
+    val simpleLoop: Seq[(Int, Int)] = Seq(
+      (1, 2),
+      (1, 3),
+      (2, 3),
+      (3, 3),
+      (3, 2),
+      (3, 1),
+      (2, 1),
+      (1, 1)
+    )
+    DayTen.buildLoop(simplestInput, (1, 2), 0) shouldBe simpleLoop
+    DayTen.buildLoop(simpleInput, (1, 2), 0) shouldBe simpleLoop
+    DayTen.buildLoop(simplestInput, (0, 1), 0) shouldBe Seq.empty
+    DayTen.buildLoop(simpleInput, (0, 1), 0) shouldBe Seq.empty
+
+    val hardLoop: Seq[(Int, Int)] = Seq(
+      (2, 1),
+      (1, 1),
+      (1, 2),
+      (0, 2),
+      (0, 3),
+      (1, 3),
+      (2, 3),
+      (2, 4),
+      (3, 4),
+      (3, 3),
+      (3, 2),
+      (3, 1),
+      (4, 1),
+      (4, 0),
+      (3, 0),
+      (2, 0)
+    )
+
+    DayTen.buildLoop(mediumInput, (2, 1), 0) shouldBe hardLoop
+    DayTen.buildLoop(hardInput, (2, 1), 0) shouldBe hardLoop
+    DayTen.buildLoop(mediumInput, (1, 0), 3) shouldBe Seq.empty
+    DayTen.buildLoop(hardInput, (1, 0), 3) shouldBe Seq.empty
+  }
 
 }

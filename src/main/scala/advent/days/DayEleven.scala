50549f16dec55023ce2dda63f1c391db695b1121
Zach Norgaard <znorgaard@bionano.com>
2023-12-13 08:36:39 -0800

    feat: solve day 11

--- src/main/scala/advent/days/DayEleven.scala
+++ src/main/scala/advent/days/DayEleven.scala
@@ -1,6 +1,92 @@
 package advent.days
 
+import scala.io.{BufferedSource, Source}
+
 object DayEleven {
+    val puzzleInput: BufferedSource = Source.fromFile("src/main/resources/DayElevenInput.txt")
+
     def main(): Unit =
-        println("Hello day eleven.")
+        val lines: Iterator[String] = puzzleInput
+          .getLines()
+        val universe: Seq[Seq[String]] = getUniverse(lines.toSeq)
+        val coordinates: Seq[(BigInt, BigInt)] = expandCoordinates(
+            coordinates = galaxyCoordinates(universe),
+            expandedRows = emptyRows(universe),
+            expandedColumns = emptyColumns(universe),
+            expansion = 1000000
+        )
+        println(coordinates)
+        println(emptyRows(universe))
+        println(emptyColumns(universe))
+        println(distanceFromCoordinates(coordinates).sum)
+
+    def partOne(): Unit =
+        val lines: Iterator[String] = puzzleInput
+          .getLines()
+        val expandedUniverse = expand(getUniverse(lines.toSeq))
+        println(allDistances(expandedUniverse).sum)
+
+    def getUniverse(lines: Seq[String]): Seq[Seq[String]] =
+        lines.map(_.split(""))
+
+    def expandRows(universe: Seq[Seq[String]]): Seq[Seq[String]] =
+        if (universe.isEmpty) {
+            Seq.empty
+        } else if (universe.head.forall(_ == ".")) {
+            Seq(universe.head, universe.head) ++ expandRows(universe.tail)
+        } else {
+            Seq(universe.head) ++ expandRows(universe.tail)
+        }
+
+    def emptyRows(universe: Seq[Seq[String]]): Seq[BigInt] =
+        universe.zipWithIndex.flatMap((row, rowIndex) => if (row.forall(_ ==".")) Some(rowIndex) else None)
+
+    def emptyColumns(universe: Seq[Seq[String]]): Seq[BigInt] =
+        universe.head.indices.flatMap(
+            i => if (universe.map(_(i)).forall(_ == ".")) Some(i) else None
+        )
+
+    def expand(universe: Seq[Seq[String]]): Seq[Seq[String]] =
+        val expandedRowUniverse = expandRows(universe)
+        val columnsToExpand: Seq[BigInt] = emptyColumns(expandedRowUniverse)
+        expandedRowUniverse.map(
+            row => {
+                row.zipWithIndex.flatMap { case (s, i) => if (columnsToExpand.contains(i)) Seq(s, s) else Seq(s) }
+            }
+        )
+
+    def galaxyCoordinates(universe: Seq[Seq[String]]): Seq[(BigInt, BigInt)] =
+        universe.zipWithIndex.flatMap((row, rowIndex) => {
+            row.zipWithIndex.flatMap((s, columnIndex) => if (s == "#") Some((rowIndex, columnIndex)) else None)
+        })
+
+    def distance(cordA: (BigInt, BigInt), cordB: (BigInt, BigInt)): BigInt =
+        val yDistance: BigInt = (cordA._1 - cordB._1).abs
+        val xDistance: BigInt = (cordA._2 - cordB._2).abs
+        xDistance + yDistance
+
+    def expandCoordinates(
+                           coordinates: Seq[(BigInt, BigInt)],
+                           expandedRows: Seq[BigInt],
+                           expandedColumns: Seq[BigInt],
+                           expansion: BigInt
+                         ): Seq[(BigInt, BigInt)] =
+        coordinates.map(
+            galaxyPosition => {
+                val rowCount: BigInt = expandedRows.count(_ < galaxyPosition._1)
+                val columnCount: BigInt = expandedColumns.count(_ < galaxyPosition._2)
+                (
+                  galaxyPosition._1 + (rowCount * expansion) - rowCount,
+                  galaxyPosition._2 + (columnCount * expansion) - columnCount
+                )
+            }
+        )
+
+    def allDistances(universe: Seq[Seq[String]]): Seq[BigInt] =
+        val coordinates: Seq[(BigInt, BigInt)] = galaxyCoordinates(universe)
+        distanceFromCoordinates(coordinates)
+
+    def distanceFromCoordinates(coordinates: Seq[(BigInt, BigInt)]): Seq[BigInt] =
+        coordinates.combinations(2).map(galaxyPair => distance(galaxyPair.head, galaxyPair.last)).toSeq
+
 }
--- src/test/scala/days/DayElevenTests.scala
+++ src/test/scala/days/DayElevenTests.scala
@@ -1,10 +1,155 @@
+package advent
 package days
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
 
 class DayElevenTests extends AnyFunSuite {
 
-  // First test
-  test("first test")(pending)
+  val testTextInput: Seq[String] = Seq(
+    "...#......",
+    ".......#..",
+    "#.........",
+    "..........",
+    "......#...",
+    ".#........",
+    ".........#",
+    "..........",
+    ".......#..",
+    "#...#....."
+  )
+
+  val testInput: Seq[Seq[String]] = Seq(
+    "...#......".split("").toSeq,
+    ".......#..".split("").toSeq,
+    "#.........".split("").toSeq,
+    "..........".split("").toSeq,
+    "......#...".split("").toSeq,
+    ".#........".split("").toSeq,
+    ".........#".split("").toSeq,
+    "..........".split("").toSeq,
+    ".......#..".split("").toSeq,
+    "#...#.....".split("").toSeq
+  )
+
+  val expandedInput: Seq[Seq[String]] = Seq(
+    "....#........".split("").toSeq,
+    ".........#...".split("").toSeq,
+    "#............".split("").toSeq,
+    ".............".split("").toSeq,
+    ".............".split("").toSeq,
+    "........#....".split("").toSeq,
+    ".#...........".split("").toSeq,
+    "............#".split("").toSeq,
+    ".............".split("").toSeq,
+    ".............".split("").toSeq,
+    ".........#...".split("").toSeq,
+    "#....#.......".split("").toSeq
+  )
+
+  test("getUniverse") {
+    DayEleven.getUniverse(testTextInput) shouldBe testInput
+  }
+  test("expandRows") {
+    val expectedOut: Seq[Seq[String]] = Seq(
+      "...#......".split("").toSeq,
+      ".......#..".split("").toSeq,
+      "#.........".split("").toSeq,
+      "..........".split("").toSeq,
+      "..........".split("").toSeq,
+      "......#...".split("").toSeq,
+      ".#........".split("").toSeq,
+      ".........#".split("").toSeq,
+      "..........".split("").toSeq,
+      "..........".split("").toSeq,
+      ".......#..".split("").toSeq,
+      "#...#.....".split("").toSeq
+    )
+    DayEleven.expandRows(testInput) shouldBe expectedOut
+  }
+
+  test("expandUniverse") {
+    DayEleven.expand(testInput) shouldBe expandedInput
+  }
+
+  test("galaxyCoordinates") {
+    val expectedOut: Seq[(BigInt, BigInt)] = Seq(
+      (0, 4),
+      (1, 9),
+      (2, 0),
+      (5, 8),
+      (6, 1),
+      (7, 12),
+      (10, 9),
+      (11, 0), (11, 5)
+    )
+
+    DayEleven.galaxyCoordinates(expandedInput) shouldBe expectedOut
+  }
+
+  test("distance") {
+    DayEleven.distance((0, 4), (10, 9)) shouldBe 15
+    DayEleven.distance((2, 0), (7, 12)) shouldBe 17
+    DayEleven.distance((11, 0), (11, 5)) shouldBe 5
+  }
+
+  test("allDistances") {
+    DayEleven.allDistances(expandedInput).sum shouldBe 374
+  }
+
+  test("emptyRows") {
+    DayEleven.emptyRows(testInput) shouldBe Seq(3, 7)
+  }
+
+  test("emptyColumns") {
+    DayEleven.emptyColumns(testInput) shouldBe Seq(2, 5, 8)
+  }
+
+  test("expandedCoordinates") {
+    val galaxyCoordinates: Seq[(BigInt, BigInt)] = Seq(
+      (0, 3),
+      (1, 7),
+      (2, 0),
+      (4, 6),
+      (5, 1),
+      (6, 9),
+      (8, 7),
+      (9, 0), (9, 4)
+    )
+    DayEleven.galaxyCoordinates(testInput) shouldBe galaxyCoordinates
+
+    DayEleven.expandCoordinates(
+      coordinates = Seq((0,3)),
+      expandedRows = Seq(3,7),
+      expandedColumns = Seq(2, 5, 8),
+      expansion = 10
+    ) shouldBe Seq((0,12))
+
+    DayEleven.expandCoordinates(
+      coordinates = Seq((5, 5)),
+      expandedRows = Seq(1, 2, 3),
+      expandedColumns = Seq(1, 2),
+      expansion = 10
+    ) shouldBe Seq((32, 23))
+
+
+    DayEleven.distanceFromCoordinates(
+      DayEleven.expandCoordinates(
+        coordinates = galaxyCoordinates,
+        expandedRows = Seq(3, 7),
+        expandedColumns = Seq(2, 5, 8),
+        expansion = 10
+      )
+    ).sum shouldBe 1030
+
+    DayEleven.distanceFromCoordinates(
+      DayEleven.expandCoordinates(
+        coordinates = galaxyCoordinates,
+        expandedRows = Seq(3, 7),
+        expandedColumns = Seq(2, 5, 8),
+        expansion = 100
+      )
+    ).sum shouldBe 8410
+  }
 
 }

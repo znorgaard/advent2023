dc19730472f6360d9c722371bed09efa539715fe
Zach Norgaard <znorgaard@bionano.com>
2023-12-13 13:10:05 -0800

    feat: solve day 12

--- src/main/scala/advent/days/DayTwelve.scala
+++ src/main/scala/advent/days/DayTwelve.scala
@@ -1,6 +1,90 @@
 package advent.days
 
+import scala.collection.mutable
+import scala.io.{BufferedSource, Source}
+
+// Adapted from a python tutorial because these were not concepts I was familiar with
+// https://www.reddit.com/r/adventofcode/comments/18hbbxe/2023_day_12python_stepbystep_tutorial_with_bonus/
+
 object DayTwelve {
+    val puzzleInput: BufferedSource = Source.fromFile("src/main/resources/DayTwelveInput.txt")
+
     def main(): Unit =
-        println("Hello day twelve.")
+        val lines: Iterator[String] = puzzleInput
+          .getLines()
+        val parsedLines: Iterator[(String, Seq[Int])] = lines.map(partTwoLines)
+        println(parsedLines.map((pattern, groupCounts) => combinations(pattern, groupCounts)).sum)
+
+    def partOne(): Unit =
+        val lines: Iterator[String] = puzzleInput
+          .getLines()
+        val parsedLines: Iterator[(String, Seq[Int])] = lines.map(parseLine)
+        println(parsedLines.map((pattern, groupCounts) => combinations(pattern, groupCounts)).sum)
+
+    def combinations(pattern: String, groupCounts: Seq[Int]): BigInt = {
+        def pound(pattern: String, groupCounts: Seq[Int]): BigInt = {
+            val nextGroupSize: Int = groupCounts.head
+            if (nextGroupSize > pattern.length) { // not enough pattern left to make the group
+                0
+            } else {
+                val potentialGroup: String = pattern.take(nextGroupSize)
+                val allDamaged: Boolean = potentialGroup.forall(s => Seq("#", "?").contains(s.toString))
+                if (!allDamaged) {
+                    0
+                } else if (allDamaged & nextGroupSize == pattern.length & groupCounts.length == 1) {
+                    1 // This is the end of the pattern and the last group and they match!
+                } else if (nextGroupSize == pattern.length) {
+                    0 // This is the end of the pattern but there are more groups to consider
+                } else {
+                    // This is not the end of the pattern
+                    val nextCharacter: String = pattern(nextGroupSize).toString
+                    if (allDamaged & nextCharacter == "#") {
+                        0 // The nextCharacter would need to be included in the group, so this doesn't work
+                    } else {
+                        // The nextCharacter is "." or "?" but must be treated as "."
+                        // Skip ahead in the pattern and groups
+                        f((pattern.drop(nextGroupSize + 1), groupCounts.tail))
+                    }
+                }
+            }
+        }
+
+        lazy val f: ((String, Seq[Int])) => BigInt = memoize {
+            case (pattern, groupCounts) if !pattern.contains("#") & groupCounts.isEmpty => 1
+            case (pattern, groupCounts) if pattern.isEmpty | groupCounts.isEmpty => 0
+            case (pattern, groupCounts) =>
+                val firstCharacter: String = pattern.head.toString
+                val firstGroup: Int = groupCounts.head
+                if (firstCharacter == "#") {
+                    pound(pattern, groupCounts)
+                } else if (firstCharacter == ".") {
+                    // No group consumed, move to next position
+                    f((pattern.tail, groupCounts))
+                } else { //? could be either, take both paths
+                    pound(pattern, groupCounts) + f((pattern.tail, groupCounts))
+                }
+        }
+        f((pattern, groupCounts))
+    }
+
+    def parseLine(line: String): (String, Seq[Int]) =
+        val splitLine: Seq[String] = line.split(" ")
+        (
+          splitLine.head,
+          splitLine.last.split(",").map(_.toInt)
+        )
+
+    def partTwoLines(line: String): (String, Seq[Int]) =
+        val splitLine = parseLine(line)
+        (
+          "\\?$".r.replaceFirstIn((splitLine._1 + "?") * 5, ""),
+          Seq.fill(5)(splitLine._2).flatten
+        )
+
+}
+
+// From this very helpful stack overflow:
+//   https://stackoverflow.com/questions/16257378/is-there-a-generic-way-to-memoize-in-scala
+def memoize[I, O](f: I => O): I => O = new mutable.HashMap[I, O]() {self =>
+    override def apply(key: I): O = self.synchronized(getOrElseUpdate(key, f(key)))
 }
--- src/test/scala/days/DayTwelveTests.scala
+++ src/test/scala/days/DayTwelveTests.scala
@@ -1,10 +1,49 @@
+package advent
 package days
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
 
 class DayTwelveTests extends AnyFunSuite {
+  val testInput: Seq[String] = Seq(
+    "???.### 1,1,3",
+    ".??..??...?##. 1,1,3",
+    "?#?#?#?#?#?#?#? 1,3,1,6",
+    "????.#...#... 4,1,1",
+    "????.######..#####. 1,6,5",
+    "?###???????? 3,2,1"
+  )
 
   // First test
-  test("first test")(pending)
+  test("parseLines") {
+    testInput.iterator.map(DayTwelve.parseLine).toSeq shouldBe Seq(
+      ("???.###", Seq(1,1,3)),
+      (".??..??...?##.", Seq(1,1,3)),
+      ("?#?#?#?#?#?#?#?", Seq(1,3,1,6)),
+      ("????.#...#...", Seq(4,1,1)),
+      ("????.######..#####.", Seq(1,6,5)),
+      ("?###????????", Seq(3,2,1))
+    )
+  }
+
+  test("combinations") {
+    DayTwelve.combinations("", Seq(1,1,3)) shouldBe 0
+    DayTwelve.combinations("...", Seq.empty) shouldBe 1
+    DayTwelve.combinations("..#.", Seq.empty) shouldBe 0
+    DayTwelve.combinations("#", Seq(1)) shouldBe 1
+    DayTwelve.combinations("#....", Seq(1,1,3)) shouldBe 0
+    DayTwelve.combinations("....", Seq(1,1,3)) shouldBe 0
+
+    DayTwelve.combinations("???.###", Seq(1,1,3)) shouldBe 1
+    DayTwelve.combinations(".??..??...?##.", Seq(1,1,3)) shouldBe 4
+    DayTwelve.combinations("?#?#?#?#?#?#?#?", Seq(1,3,1,6)) shouldBe 1
+    DayTwelve.combinations("????.#...#...", Seq(4,1,1)) shouldBe 1
+    DayTwelve.combinations("????.######..#####.", Seq(1,6,5)) shouldBe 4
+    DayTwelve.combinations("?###????????", Seq(3,2,1)) shouldBe 10
+  }
+
+  test("partTwoLines") {
+    DayTwelve.partTwoLines(".# 1") shouldBe (".#?.#?.#?.#?.#", Seq(1, 1, 1, 1, 1))
+  }
 
 }

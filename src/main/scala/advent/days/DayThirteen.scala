299fdc752a1eed0d5cef18da04c1ce21d1bf2785
Zach Norgaard <znorgaard@bionano.com>
2023-12-14 12:51:49 -0800

    feat: solve day 13

--- src/main/scala/advent/days/DayThirteen.scala
+++ src/main/scala/advent/days/DayThirteen.scala
@@ -1,6 +1,106 @@
 package advent.days
 
+import scala.io.{BufferedSource, Source}
+import scala.collection.BufferedIterator
+
 object DayThirteen {
+    val puzzleInput: BufferedSource = Source.fromFile("src/main/resources/DayThirteenInput.txt")
+
     def main(): Unit =
-        println("Hello day thirteen.")
+        val lines: BufferedIterator[String] = puzzleInput
+          .getLines().buffered
+        val patternSeq = getPatterns(lines)
+        println(patternSeq.map(patternValue).sum)
+        println(patternSeq.map(newPatternValue).sum)
+
+    val ash: String = "."
+    val rock: String = "#"
+
+    def reflectionIndices(patternRow: String): Seq[Int] =
+        (1 until patternRow.length).flatMap(
+            i => {
+                val sliceSize = Seq(i, patternRow.length - i).min
+                if (patternRow.slice(i - sliceSize, i) == patternRow.slice(i, i + i).reverse) Some(i) else None
+            }
+        )
+
+    def findReflections(pattern: Seq[String]): Option[Int] =
+        val rowReflections: Seq[Seq[Int]] = pattern.map(reflectionIndices)
+        val allReflections: Seq[Int] = rowReflections.flatten.distinct
+        rowReflections.foldLeft(allReflections)((x, y) => x.intersect(y)).headOption
+
+
+    def transformPattern(pattern: Seq[String]): Seq[String] =
+        pattern.head.indices.map(
+            i => pattern.map(patternRow => patternRow(i)).mkString
+        )
+
+    def patternValue(pattern: Seq[String]): Int =
+        val maybeRowReflection = findReflections(pattern)
+        if (maybeRowReflection.nonEmpty) {
+            maybeRowReflection.get
+        } else {
+            findReflections(transformPattern(pattern)).get * 100
+        }
+
+    def getPatterns(lines: BufferedIterator[String]): Seq[Seq[String]] =
+        if (!lines.hasNext) {
+            Seq.empty
+        } else {
+            Seq(lines.takeWhile(_.nonEmpty).toSeq) ++ getPatterns(lines)
+        }
+
+    // part two
+    // for each row, switch each symbol in each position, see if the new reflections indices match a different value
+    def getCandidateReflections(reflections: Seq[Seq[Int]], exclude: Option[Int]): Seq[Int] =
+        val remainingReflections = if (exclude.isEmpty) {
+            reflections
+        } else {
+            reflections.map(rowReflections => rowReflections.filter(_ != exclude.get))
+        }
+        remainingReflections
+          .flatten
+          .groupBy(identity)
+          .view.mapValues(_.size)
+          .filter(_._2 == (reflections.length - 1))
+          .keys
+          .toSeq
+
+    def rowsToFix(reflections: Seq[Seq[Int]], candidateReflections: Seq[Int]): Seq[Int] =
+        candidateReflections.flatMap(
+                reflectionAxis => {
+                    reflections.zipWithIndex.flatMap {
+                        (rowReflections, i) =>
+                        if (rowReflections.contains(reflectionAxis)) { None } else {Some(i)}
+                    }
+                }
+            )
+
+    def alternateReflectionIndices(rowPattern: String): Seq[Seq[Int]] =
+        val alternateMap: Map[String, String] = Map("." -> "#", "#" -> ".")
+        val alternateRows = rowPattern.indices.map(
+            i => rowPattern.take(i) + alternateMap(rowPattern(i).toString) + rowPattern.drop(i+1)
+        )
+        alternateRows.map(reflectionIndices)
+
+    def newReflection(pattern: Seq[String]): Option[Int] =
+        val maybeReflection: Option[Int] = findReflections(pattern)
+        val reflections: Seq[Seq[Int]] = pattern.map(reflectionIndices)
+        val candidateReflections: Seq[Int] = getCandidateReflections(reflections, maybeReflection)
+        val rowIndices: Seq[Int] = rowsToFix(reflections, candidateReflections)
+        rowIndices.flatMap(
+            i => {
+                alternateReflectionIndices(pattern(i)).flatten.filter(r => candidateReflections.contains(r))
+            }
+        ).headOption
+
+
+    def newPatternValue(pattern: Seq[String]): Int =
+        val maybeRowReflection = newReflection(pattern)
+        if (maybeRowReflection.nonEmpty) {
+            maybeRowReflection.get
+        } else {
+            newReflection(transformPattern(pattern)).get * 100
+        }
+
 }
--- src/test/scala/days/DayThirteenTests.scala
+++ src/test/scala/days/DayThirteenTests.scala
@@ -1,10 +1,146 @@
+package advent
 package days
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
 
 class DayThirteenTests extends AnyFunSuite {
 
+  val testTextInput: Seq[String] = Seq(
+    "#.##..##.",
+    "..#.##.#.",
+    "##......#",
+    "##......#",
+    "..#.##.#.",
+    "..##..##.",
+    "#.#.##.#.",
+    "",
+    "#...##..#",
+    "#....#..#",
+    "..##..###",
+    "#####.##.",
+    "#####.##.",
+    "..##..###",
+    "#....#..#"
+  )
+  val testInput: Seq[Seq[String]] = Seq(
+    Seq(
+      "#.##..##.",
+      "..#.##.#.",
+      "##......#",
+      "##......#",
+      "..#.##.#.",
+      "..##..##.",
+      "#.#.##.#."
+    ),
+    Seq(
+      "#...##..#",
+      "#....#..#",
+      "..##..###",
+      "#####.##.",
+      "#####.##.",
+      "..##..###",
+      "#....#..#"
+    )
+  )
+
   // First test
-  test("first test")(pending)
+  test("getPatterns") {
+    DayThirteen.getPatterns(testTextInput.iterator.buffered) shouldBe testInput
+  }
+
+  test("reflectionIndices") {
+    DayThirteen.reflectionIndices("#.##..##.") shouldBe Seq(5, 7)
+    DayThirteen.reflectionIndices("..#.##.#.") shouldBe Seq(1, 5)
+    DayThirteen.reflectionIndices("##......#") shouldBe Seq(1, 5)
+    DayThirteen.reflectionIndices("##......#") shouldBe Seq(1, 5)
+    DayThirteen.reflectionIndices("..#.##.#.") shouldBe Seq(1, 5)
+    DayThirteen.reflectionIndices("..##..##.") shouldBe Seq(1, 3, 5, 7)
+    DayThirteen.reflectionIndices("#.#.##.#.") shouldBe Seq(5)
+  }
+
+  test("findReflections") {
+    DayThirteen.findReflections(testInput.head) shouldBe Some(5)
+    DayThirteen.findReflections(testInput.last) shouldBe None
+  }
+
+  test("transformPattern") {
+    DayThirteen.transformPattern(testInput.head) shouldBe Seq(
+      "#.##..#",
+      "..##...",
+      "##..###",
+      "#....#.",
+      ".#..#.#",
+      ".#..#.#",
+      "#....#.",
+      "##..###",
+      "..##..."
+    )
+  }
+
+  test("patternValue") {
+    testInput.map(DayThirteen.patternValue) shouldBe Seq(5, 400)
+  }
+
+  test("getCandidateReflections") {
+    DayThirteen.getCandidateReflections(
+      Seq(Seq(1,2), Seq(1,3), Seq(1,4), Seq(1,5), Seq(1,6)),
+      Some(1)
+    ) shouldBe Seq.empty
+
+    DayThirteen.getCandidateReflections(
+      Seq(Seq(1, 2), Seq(1, 2), Seq(1, 2), Seq(1, 2), Seq(1, 6)),
+      Some(1)
+    ) shouldBe Seq(2)
+    DayThirteen.getCandidateReflections(
+      Seq(Seq(1, 2, 3), Seq(1, 2, 3), Seq(1, 2, 3), Seq(1, 2), Seq(1, 6, 3)),
+      Some(1)
+    ) shouldBe Seq(2, 3)
+  }
+
+  test("rowsToFix") {
+    DayThirteen.rowsToFix(
+      reflections = Seq(Seq(1, 2), Seq(2, 3), Seq(2)),
+      candidateReflections = Seq.empty
+    ) shouldBe Seq.empty
+    DayThirteen.rowsToFix(
+      reflections = Seq(Seq(1, 2), Seq(2, 3), Seq(2, 4)),
+      candidateReflections = Seq.empty
+    ) shouldBe Seq.empty
+    DayThirteen.rowsToFix(
+      reflections = Seq(Seq(1), Seq(1, 3), Seq(3)),
+      candidateReflections = Seq(3)
+    ) shouldBe Seq(0)
+    DayThirteen.rowsToFix(
+      reflections = Seq(Seq(1, 3), Seq(2, 3), Seq(2, 3), Seq(1, 3), Seq(1)),
+      candidateReflections = Seq(3)
+    ) shouldBe Seq(4)
+  }
+
+  test("alternateReflectionIndices") {
+    DayThirteen.alternateReflectionIndices("#.##..##.") shouldBe Seq(
+      Seq(1, 3, 5, 7), // ..##..##.
+      Seq(1, 2, 7),    // ####..##.
+      Seq(2, 7),       // #..#..##.
+      Seq(7),          // #.#...##.
+      Seq(7),          // #.###.##.
+      Seq(3),          // #.##.###.
+      Seq.empty,       // #.##...#.
+      Seq(8),          // #.##..#..
+      Seq(8),          // #.##..###
+    )
+  }
+
+  test("newReflection") {
+    DayThirteen.newReflection(testInput.head) shouldBe None
+    DayThirteen.newReflection(testInput.last) shouldBe None
+
+    DayThirteen.newReflection(DayThirteen.transformPattern(testInput.head)) shouldBe Some(3)
+    DayThirteen.newReflection(DayThirteen.transformPattern(testInput.last)) shouldBe Some(1)
+  }
+
+  test("newPatternValue") {
+    testInput.map(DayThirteen.newPatternValue) shouldBe Seq(300, 100)
+  }
 
 }

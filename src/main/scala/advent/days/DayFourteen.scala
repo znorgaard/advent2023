f6377f276a136f643600607ac78d740d79508348
Zach Norgaard <znorgaard@bionano.com>
2023-12-18 08:14:30 -0800

    feat: solve day 14 part 1

--- src/main/scala/advent/days/DayFourteen.scala
+++ src/main/scala/advent/days/DayFourteen.scala
@@ -1,6 +1,43 @@
 package advent.days
 
+import scala.io.{BufferedSource, Source}
+
 object DayFourteen {
+    val puzzleInput: BufferedSource = Source.fromFile("src/main/resources/DayFourteenInput.txt")
+
     def main(): Unit =
-        println("Hello day fourteen.")
+        val lines: Seq[String] = puzzleInput
+          .getLines().toSeq
+        println(getColumns(lines).map(roll).map(columnLoad).sum)
+
+    def getColumns(lines: Seq[String]): Seq[String] =
+        lines.head.indices.map(
+            i => lines.map(_(i)).mkString
+        )
+    def columnLoad(column: String): Int =
+        column.zipWithIndex.map((c, i) => if (c.toString == "O") {column.length - i} else {0}).sum
+
+    def roll(column: String): String =
+        // Need to find positions of cubes, "#", and round rocks "O"
+        val cubePos = "#".r.findAllMatchIn(column).map(_.start).toSeq
+        val roundPos = "O".r.findAllMatchIn(column).map(_.start).toSeq
+        if (cubePos.isEmpty) {
+            "O" * roundPos.length + "." * (column.length - roundPos.length)
+        } else {
+            val stopPos = Seq(-1) ++ cubePos ++ Seq(column.length + 1)
+            val roundGroupSizes = stopPos.indices.tail.map(
+                i => {
+                    roundPos.count(roundIndex => roundIndex > stopPos(i - 1) & roundIndex < stopPos(i))
+                }
+            )
+            val columnStart = cubePos.indices.map(
+                i => {
+                    val previousCubePos = if (i == 0) {0} else {cubePos(i - 1) + 1}
+                    val dotCount = cubePos(i) - roundGroupSizes(i) - previousCubePos
+                    "O" * roundGroupSizes(i) + "." * dotCount + "#"
+                }
+            ).mkString
+            val lastRounds = if (cubePos.length == roundGroupSizes.length) {""} else {"O" * roundGroupSizes.last}
+            columnStart + lastRounds + "." * (column.length - columnStart.length - lastRounds.length)
+        }
 }
--- src/test/scala/days/DayFourteenTests.scala
+++ src/test/scala/days/DayFourteenTests.scala
@@ -1,10 +1,60 @@
+package advent
 package days
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
 
 class DayFourteenTests extends AnyFunSuite {
+  val testInput: Seq[String] = Seq(
+    "O....#....", // 10
+    "O.OO#....#", //  9
+    ".....##...", //  8
+    "OO.#O....O", //  7
+    ".O.....O#.", //  6
+    "O.#..O.#.#", //  5
+    "..O..#O..O", //  4
+    ".......O..", //  3
+    "#....###..", //  2
+    "#OO..#...."  //  1
+  )
+
+  val testColumns: Seq[String] = Seq(
+    "OO.O.O..##",
+    "...OO....O",
+    ".O...#O..O",
+    ".O.#......",
+    ".#.O......",
+    "#.#..O#.##",
+    "..#...O.#.",
+    "....O#.O#.",
+    "....#.....",
+    ".#.O.#O..."
+  )
+
+  val rolledColumns: Seq[String] = Seq(
+    "OOOO....##",
+    "OOO.......",
+    "O....#OO..",
+    "O..#......",
+    ".#O.......",
+    "#.#O..#.##",
+    "..#O....#.",
+    "O....#O.#.",
+    "....#.....",
+    ".#O..#O..."
+  )
 
   // First test
-  test("first test")(pending)
+  test("getColumns") {
+    DayFourteen.getColumns(testInput) shouldBe testColumns
+  }
+
+  test("columnLoad") {
+    testColumns.map(DayFourteen.columnLoad) shouldBe Seq(31, 14, 14, 9, 7, 5, 4, 9, 0, 11)
+  }
+
+  test("roll") {
+    testColumns.map(DayFourteen.roll) shouldBe rolledColumns
+  }
 
 }

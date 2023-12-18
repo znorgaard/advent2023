3a27a9521c4abf89c9b10b41682d92aec63a07a6
Zach Norgaard <znorgaard@bionano.com>
2023-12-18 15:41:03 -0800

    feat: solve day 15

--- src/main/scala/advent/days/DayFifteen.scala
+++ src/main/scala/advent/days/DayFifteen.scala
@@ -1,6 +1,70 @@
 package advent.days
 
+import scala.io.{BufferedSource, Source}
 object DayFifteen {
+    val puzzleInput: BufferedSource = Source.fromFile("src/main/resources/DayFifteenInput.txt")
+
     def main(): Unit =
-        println("Hello day fifteen.")
+        val lines: Iterator[String] = puzzleInput
+          .getLines()
+        println(foldSteps(getInstructions(lines.next)))
+
+    def partOne(): Unit =
+        val lines: Iterator[String] = puzzleInput
+          .getLines()
+        val asciiSeqs = getInput(lines)
+        println(asciiSeqs.map(asciiSeq => hash(asciiSeq)).sum)
+
+    def getInput(lines: Iterator[String]): Seq[Seq[Int]] =
+        lines.next.split(",").map(_.map(_.toInt))
+
+    def getInstructions(line: String): Seq[String] =
+        line.split(",")
+
+    def parseStep(step: String): (String, String, Option[Int]) =
+        val modification: String = if (step.contains("=")) "=" else "-"
+        val splitStep: Seq[String] = step.split(modification)
+        val lens: Option[Int] = if (splitStep.tail.nonEmpty) Some(splitStep.last.toInt) else None
+        (splitStep.head, modification, lens)
+
+    def updateBoxes(boxMap: Map[Int, Seq[(String, Int)]], step: String): Map[Int, Seq[(String, Int)]] =
+            val (label, mod, lens) = parseStep(step)
+            val labelHash: Int = hashLabel(label)
+            if (mod == "-") {
+                boxMap + (labelHash -> boxMap(labelHash).filter(_._1 != label))
+            } else if (!boxMap(labelHash).exists(_._1 == label)) {
+                boxMap + (labelHash -> (boxMap(labelHash) :+ (label, lens.get)))
+            } else {
+                val newBox = boxMap(labelHash).map(
+                    (oldLabel, oldLens) => {
+                        val newLens = if (oldLabel == label) lens.get else oldLens
+                        (oldLabel, newLens)
+                    }
+                )
+                boxMap + (labelHash -> newBox)
+            }
+
+    def boxPower(boxNumber: Int, lenses: Seq[(String, Int)]): Int =
+        lenses.zipWithIndex.map(
+            (labelLensPair, i) => (1 + boxNumber) * (1 + i) * labelLensPair._2
+        ).sum
+
+    def foldSteps(instructions: Seq[String]): Int =
+        val startingMap: Map[Int, Seq[(String, Int)]] = (0 to 255).map(_ -> Seq.empty).toMap
+        val boxes: Map[Int, Seq[(String, Int)]] = instructions.foldLeft(startingMap)(
+            (boxMap, step) => updateBoxes(boxMap, step)
+        )
+        boxes.map((boxNumber, lenses) => boxPower(boxNumber, lenses)).sum
+
+
+
+    def hash(asciiSeq: Seq[Int]): Int =
+        asciiSeq.foldLeft(0)(
+            (a, b) => {
+                (a + b) * 17 % 256
+            }
+        )
+
+    def hashLabel(label: String): Int =
+        hash(label.map(_.toInt))
 }
--- src/test/scala/days/DayFifteenTests.scala
+++ src/test/scala/days/DayFifteenTests.scala
@@ -1,10 +1,81 @@
+package advent
 package days
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
 
 class DayFifteenTests extends AnyFunSuite {
+  val testInput: String = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+  val testFileLine: Seq[String] = Seq(testInput)
 
   // First test
-  test("first test")(pending)
+  test("getInput") {
+    DayFifteen.getInput(Seq("HASH").iterator) shouldBe Seq(
+      Seq(72, 65, 83, 72)
+    )
+    DayFifteen.getInput(testFileLine.iterator) shouldBe Seq(
+      Seq(114, 110, 61, 49),
+      Seq(99, 109, 45),
+      Seq(113, 112, 61, 51),
+      Seq(99, 109, 61, 50),
+      Seq(113, 112, 45),
+      Seq(112, 99, 61, 52),
+      Seq(111, 116, 61, 57),
+      Seq(97, 98, 61, 53),
+      Seq(112, 99, 45),
+      Seq(112, 99, 61, 54),
+      Seq(111, 116, 61, 55)
+    )
+  }
+
+  test("hash") {
+    DayFifteen.hash(Seq(72, 65, 83, 72)) shouldBe 52
+    DayFifteen.getInput(testFileLine.iterator).map(asciiSeq => DayFifteen.hash(asciiSeq)) shouldBe Seq(
+      30, 253, 97, 47, 14, 180, 9, 197, 48, 214, 231
+    )
+    DayFifteen.getInput(testFileLine.iterator).map(asciiSeq => DayFifteen.hash(asciiSeq)).sum shouldBe 1320
+  }
+
+  test("hashLabel") {
+    DayFifteen.hashLabel("HASH") shouldBe 52
+  }
+
+  test("parseStep") {
+    DayFifteen.parseStep("rn=1") shouldBe ("rn", "=", Some(1))
+    DayFifteen.parseStep("cm-") shouldBe ("cm", "-", None)
+  }
+
+  test("getInstructions") {
+    DayFifteen.getInstructions("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7") shouldBe Seq(
+      "rn=1", "cm-", "qp=3", "cm=2", "qp-", "pc=4", "ot=9", "ab=5", "pc-", "pc=6", "ot=7"
+    )
+  }
+
+  test("boxPower") {
+    DayFifteen.boxPower(0, Seq(("rn", 1), ("cm", 2))) shouldBe 5
+    DayFifteen.boxPower(3, Seq(("ot", 7), ("ab", 5), ("pc", 6))) shouldBe 140
+  }
+
+  test("foldSteps") {
+    val steps = Seq("rn=1", "cm-", "qp=3", "cm=2", "qp-", "pc=4", "ot=9", "ab=5", "pc-", "pc=6", "ot=7")
+    DayFifteen.foldSteps(steps) shouldBe 145
+  }
+
+  test("updateBoxes") {
+    DayFifteen.updateBoxes(Map(0-> Seq.empty), "rn=1") shouldBe Map(0 -> Seq(("rn", 1)))
+    DayFifteen.updateBoxes(
+      boxMap = Map(0 -> Seq(("rn", 1))),
+      step   = "cm-"
+    ) shouldBe Map(0 -> Seq(("rn", 1)))
+    DayFifteen.updateBoxes(
+      boxMap = Map(0 -> Seq(("rn", 1)), 1 -> Seq.empty),
+      step = "qp=3"
+    ) shouldBe Map(0 -> Seq(("rn", 1)), 1-> Seq(("qp", 3)))
+    DayFifteen.updateBoxes(
+      boxMap = Map(0 -> Seq(("rn", 1)), 1 -> Seq(("qp", 3))),
+      step = "cm=2"
+    ) shouldBe Map(0 -> Seq(("rn", 1), ("cm", 2)), 1 -> Seq(("qp", 3)))
+
+  }
 
 }
